d50cd12251d237092ec8e172916be3bfed16da67
Thomas <mergener.thomas@gmail.com>
2022-09-22 01:28:32 -0300

    Removed 'install' step from lunachess::initialze.

--- src/lunachess.cpp
+++ src/lunachess.cpp
@@ -11,22 +11,12 @@ namespace lunachess {
 
 static bool s_Initialized = false;
 
-static void install() {
-    namespace fs = std::filesystem;
-
-    fs::create_directories("data/scores");
-    fs::create_directories("data/trainings");
-}
-
 void initializeEverything() {
     // Prevent multiple setup calls
     if (s_Initialized) {
         return;
     }
     s_Initialized = true;
-
-    // Create necessary directories
-    install();
 
     // Initialize core functionalities
     zobrist::initialize();
