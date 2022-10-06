da80321e0345bac046592c9fbdc3263a0a3c9bf4
Thomas <mergener.thomas@gmail.com>
2022-10-06 16:01:02 -0300

    Added test cases for the new supported endgame types.

--- src/lunatest/tests/endgame.cpp
+++ src/lunatest/tests/endgame.cpp
@@ -33,7 +33,12 @@ std::vector<TestCase> endgameTests = {
     EndgameTest("8/8/3k4/8/8/2PK4/8/8 w - - 0 1", EG_KP_K, CL_WHITE),
     EndgameTest("8/8/2pk4/8/8/3K4/8/8 b - - 0 1", EG_KP_K, CL_BLACK),
     EndgameTest("8/8/3k4/6B1/8/3K4/1N6/8 b - - 0 1", EG_KBN_K, CL_WHITE),
-    EndgameTest("8/8/1bnk4/8/8/3K4/8/8 b - - 0 1", EG_KBN_K, CL_BLACK)
+    EndgameTest("8/8/1bnk4/8/8/3K4/8/8 b - - 0 1", EG_KBN_K, CL_BLACK),
+    EndgameTest("8/8/3k4/8/8/8/3RK3/8 b - - 0 1", EG_KR_K, CL_WHITE),
+    EndgameTest("8/8/3k1r2/8/8/8/4K3/8 w - - 0 1", EG_KR_K, CL_BLACK),
+    EndgameTest("8/8/3k4/8/7Q/8/4K3/8 w - - 0 1", EG_KQ_K, CL_WHITE),
+    EndgameTest("8/8/3k4/8/7q/8/4K3/8 w - - 0 1", EG_KQ_K, CL_BLACK),
+    EndgameTest("8/8/3k4/8/6pq/8/4K3/8 w - - 0 1", EG_UNKNOWN, CL_ANY),
 };
 
 }
