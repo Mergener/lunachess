7195912be928cde916ae431bc313b6224d766cda
Thomas <mergener.thomas@gmail.com>
2023-11-01 00:36:57 -0300

    AI types module.

---
+++ src/luna/ai/aitypes.h
@@ -0,0 +1,14 @@
+#ifndef LUNA_AI_AITYPES_H
+#define LUNA_AI_AITYPES_H
+
+namespace lunachess::ai {
+
+enum ScoreType {
+    ST_EXACT,
+    ST_LOWERBOUND,
+    ST_UPPERBOUND
+};
+
+}
+
+#endif // LUNA_AI_AITYPES_H
